fdcb97168aa309e704e889316e2fb323a6406eea
Bonomani <8841264+bonomani@users.noreply.github.com>
2026-03-21 21:08:12 +0100

    Update ariaflow formula to alpha 35

--- Formula/ariaflow.rb
+++ Formula/ariaflow.rb
@@ -1,9 +1,9 @@
 class Ariaflow < Formula
   desc "Sequential aria2 queue driver with adaptive bandwidth control"
   homepage "https://github.com/bonomani/ariaflow"
-  url "https://github.com/bonomani/ariaflow/archive/refs/tags/v0.1.1-alpha.34.tar.gz"
-  sha256 "12eb4009f7d63793c205b1ea08cf345acfd3421999c865394775ac29bec7a6b2"
-  version "0.1.1-alpha.34"
+  url "https://github.com/bonomani/ariaflow/archive/refs/tags/v0.1.1-alpha.35.tar.gz"
+  sha256 "7c4a7bd863b3dc1734d5c6591a790ccea12d9d1cbe198a5d9d4fe0d8c26162bd"
+  version "0.1.1-alpha.35"
   license "MIT"
   head "https://github.com/bonomani/ariaflow.git", branch: "master"
 
