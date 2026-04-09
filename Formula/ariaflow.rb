3c0fbba47d4ddeaf52f228beb23db8dc9bddd3f9
Bonomani <8841264+bonomani@users.noreply.github.com>
2026-04-09 15:56:39 +0200

    Update ariaflow-server formula to v0.1.163

--- Formula/ariaflow.rb
+++ Formula/ariaflow.rb
@@ -1,9 +1,9 @@
 class Ariaflow < Formula
   desc "Install ariaflow server and dashboard together"
   homepage "https://github.com/bonomani/ariaflow-server"
-  url "https://github.com/bonomani/ariaflow-server/archive/refs/tags/v0.1.152.tar.gz"
-  sha256 "4c52fbde26e8110b0ce4dd805a9a20bcffeae7f9ca4dbef7fddc38a7a9153155"
-  version "0.1.152"
+  url "https://github.com/bonomani/ariaflow-server/archive/refs/tags/v0.1.163.tar.gz"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  version "0.1.163"
   license "MIT"
 
   depends_on "ariaflow-server"
